905d9441b108963b2823328690e2ebb73ef035fb
Kovid Tripathi <officialkovid@gmail.com>
2025-02-18 03:20:16 +0530

    add contact form functionlity

--- src/apps/contact/views.py
+++ src/apps/contact/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render,redirect
-from .models import *
-from django.contrib import messages  # Add this import
-from django.http import HttpResponse
-# Create your views here.
+from .models import ContactMessage
+from django.contrib import messages  
+
 
 def contact(request):
     if request.method == "POST":
         data = request.POST
         fullName = data.get('full_name')
         email = data.get('email')
-        phone = data.get('phone')  # Add this line
+        phone = data.get('phone')  
         message = data.get('message')
         
         ContactMessage.objects.create(
             full_name=fullName,
             email=email,
-            phone=phone,  # Add this line
+            phone=phone,  
             message=message,
         )
         messages.success(request, "Thank you for contacting us! Our customer service team will get back to you shortly.")
