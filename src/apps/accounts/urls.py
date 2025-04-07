79e6e9136615bc433ce1572261f36180d5a4970f
Kovid Tripathi <officialkovid@gmail.com>
2025-04-08 00:37:53 +0530

    make the index page Inherit from base

--- src/apps/accounts/urls.py
+++ src/apps/accounts/urls.py
@@ -3,8 +3,7 @@ from .views import signup, login_page, logout_user, account
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login/', login_page, name='login'),
-    path('', login_page, name='login'),
+    path('login/', login_page, name='login'),  # Keep only this login path
     path('logout/', logout_user, name='logout'),
     path('account/', account, name='account'),
 ]
